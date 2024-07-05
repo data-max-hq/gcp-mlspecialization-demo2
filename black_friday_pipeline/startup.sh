8bcc69b3f88b8c8abfd22ff62222f55f30a2935b
Arbs28 <87767366+Arbs28@users.noreply.github.com>
2024-07-05 10:38:17 +0200

    added startup script

---
+++ black_friday_pipeline/startup.sh
@@ -0,0 +1,17 @@
+sudo apt update
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+curl https://pyenv.run | bash
+echo -e 'export PYENV_ROOT="$HOME/.pyenv"\nexport PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'eval "$(pyenv init --path)"\neval "$(pyenv init -)"' >> ~/.bashrc
+exec "$SHELL"
+pyenv --version
+pyenv install 3.10.12
+pyenv global 3.10.12
+sudo apt install git
+git clone https://github.com/data-max-hq/gcp-mlspecialization-demo2.git
+cd gcp-mlspecialization-demo2
+python3 -m vent venv
+source vent/bin/activate
+pip install requirements.txt
+cd black_friday_pipeline
+python -m pipeline/run_pipeline
