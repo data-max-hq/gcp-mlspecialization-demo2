77d79d1ed7741e4b6c76ff812c76773f47bb8941
Ergin Gorishti <ergingorishti@gmail.com>
2024-07-08 22:02:44 +0200

    Minor changes

--- black_friday_pipeline/components/model_evaluator_and_pusher.py
+++ black_friday_pipeline/components/model_evaluator_and_pusher.py
@@ -1,5 +1,7 @@
 from tfx.components import Evaluator, Pusher
 from tfx.proto import pusher_pb2, evaluator_pb2
+from tfx.proto import model_eval_lib_pb2 as me_proto
+
 
 def create_evaluator_and_pusher(example_gen, trainer, serving_model_dir):
     evaluator = Evaluator(
@@ -7,6 +9,9 @@ def create_evaluator_and_pusher(example_gen, trainer, serving_model_dir):
         model=trainer.outputs['model'],
         feature_slicing_spec=evaluator_pb2.FeatureSlicingSpec(
             specs=[evaluator_pb2.SingleSlicingSpec(column_for_slicing=['Gender'])]
+        ),
+        eval_config=me_proto.EvalConfig(
+            model_specs=[me_proto.ModelSpec(signature_name='serving_default')]
         )
     )
 
