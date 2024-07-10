7bb63383b04bb78f5e7f029e5240c83cb105c3d6
Ergin Gorishti <ergingorishti@gmail.com>
2024-07-10 14:52:53 +0200

    Minor changes

--- black_friday_pipeline/pipeline/run_pipeline.py
+++ black_friday_pipeline/pipeline/run_pipeline.py
@@ -17,7 +17,7 @@ MODULE_ROOT = os.getenv("MODULE_ROOT")
 SERVING_MODEL_DIR = os.getenv("SERVING_MODEL_DIR")
 GCS_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME")
 
-PIPELINE_DEFINITION_FILE = GCS_BUCKET_NAME + "/" + PIPELINE_NAME + '_pipeline.json'
+PIPELINE_DEFINITION_FILE = PIPELINE_NAME + '_pipeline.json'
 
 runner = tfx.orchestration.experimental.KubeflowV2DagRunner(
     config=tfx.orchestration.experimental.KubeflowV2DagRunnerConfig(),
