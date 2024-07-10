1ce31f0eff2c5f83bbbf21902749f5593cb55773
Ergin Gorishti <ergingorishti@gmail.com>
2024-07-10 17:35:11 +0200

    Minor changes

--- black_friday_pipeline/pipeline/pipeline_definition.py
+++ black_friday_pipeline/pipeline/pipeline_definition.py
@@ -16,7 +16,7 @@ def create_pipeline(pipeline_name: str, pipeline_root: str, data_path: str, serv
     transform = create_transform(example_gen, schema_gen)
     trainer = create_trainer(transform, schema_gen, module_file)
     evaluator, pusher = create_evaluator_and_pusher(transform, trainer, serving_model_dir)
-    model_uri = pusher.outputs['pushed_model'].uri
+    model_uri = pusher.outputs['pushed_model'].get().uri
     model_register_and_deployer = create_register_and_deployer(model_uri)
 
     return pipeline.Pipeline(
