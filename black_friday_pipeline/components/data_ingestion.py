14da36ddcf2a07ac786f275ac1a2c8c87b2af1cd
Ergin Gorishti <ergingorishti@gmail.com>
2024-07-11 11:31:56 +0200

    Minor changes

--- black_friday_pipeline/components/data_ingestion.py
+++ black_friday_pipeline/components/data_ingestion.py
@@ -1,14 +1,14 @@
 from tfx.components import CsvExampleGen
 from tfx.proto import example_gen_pb2
 from tfx.v1.proto import Output
-from tfx import proto
+from tfx.v1.proto import SplitConfig 
 
 def create_example_gen(input_base: str):
     output = Output(
              split_config=example_gen_pb2.SplitConfig(splits=[
-                 proto.SplitConfig.Split(name='train', hash_buckets=8),
-                 proto.SplitConfig.Split(name='eval', hash_buckets=1),
-                 proto.SplitConfig.Split(name='test', hash_buckets=1)
+                SplitConfig.Split(name='train', hash_buckets=8),
+                SplitConfig.Split(name='eval', hash_buckets=1),
+                SplitConfig.Split(name='test', hash_buckets=1)
              ]))
     
     example_gen = CsvExampleGen(input_base=input_base, output_config=output)
