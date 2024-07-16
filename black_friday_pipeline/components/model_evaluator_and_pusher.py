6c4a8f826536c439d06b0f9b35733a531d36fff9
Ergin Gorishti <ergingorishti@gmail.com>
2024-07-16 15:20:40 +0200

    Minor changes

--- black_friday_pipeline/components/model_evaluator_and_pusher.py
+++ black_friday_pipeline/components/model_evaluator_and_pusher.py
@@ -1,6 +1,17 @@
 from tfx.components import Evaluator, Pusher
-from tfx.proto import pusher_pb2, evaluator_pb2 
+from tfx.proto import pusher_pb2, evaluator_pb2
+from tfx import v1 as tfx
 import tensorflow_model_analysis as tfma
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
+endpoint_name = os.getenv("SERVING_ENDPOINT_NAME")
+region = os.getenv("GOOGLE_CLOUD_REGION")
+
+
 
 
 eval_config = tfma.EvalConfig(
@@ -25,6 +36,22 @@ eval_config = tfma.EvalConfig(
     )
 
 
+vertex_serving_spec = {
+      'project_id': project_id,
+      'endpoint_name': endpoint_name,
+      # Remaining argument is passed to aiplatform.Model.deploy()
+      # See https://cloud.google.com/vertex-ai/docs/predictions/deploy-model-api#deploy_the_model
+      # for the detail.
+      #
+      # Machine type is the compute resource to serve prediction requests.
+      # See https://cloud.google.com/vertex-ai/docs/predictions/configure-compute#machine-types
+      # for available machine types and acccerators.
+      'machine_type': 'n1-standard-2',
+  }
+
+serving_image = "europe-docker.pkg.dev/vertex-ai-restricted/prediction/tf_opt-cpu.2-13:latest"
+
+
 def create_evaluator_and_pusher(example_gen, trainer, serving_model_dir):
     evaluator = Evaluator(
         examples=example_gen.outputs['examples'],
@@ -33,13 +60,16 @@ def create_evaluator_and_pusher(example_gen, trainer, serving_model_dir):
         example_splits=['test']
     )
 
-    pusher = Pusher(
-        model=trainer.outputs['model'],
-        model_blessing=evaluator.outputs['blessing'],
-        push_destination=pusher_pb2.PushDestination(
-            filesystem=pusher_pb2.PushDestination.Filesystem(
-                base_directory=serving_model_dir
-            )
-        )
-    )
+    pusher = tfx.extensions.google_cloud_ai_platform.Pusher(
+      model=trainer.outputs['model'],
+      custom_config={
+          tfx.extensions.google_cloud_ai_platform.ENABLE_VERTEX_KEY:
+              True,
+          tfx.extensions.google_cloud_ai_platform.VERTEX_REGION_KEY:
+              region,
+          tfx.extensions.google_cloud_ai_platform.VERTEX_CONTAINER_IMAGE_URI_KEY:
+              serving_image,
+          tfx.extensions.google_cloud_ai_platform.SERVING_ARGS_KEY:
+            vertex_serving_spec,
+      })
     return evaluator, pusher
