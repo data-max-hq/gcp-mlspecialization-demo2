605cf54238b5030246c91552aedb7c2119e42c21
Ergin Gorishti <ergingorishti@gmail.com>
2024-07-25 13:23:31 +0200

    Minor changes

--- black_friday_pipeline/components/model_evaluator_and_pusher.py
+++ black_friday_pipeline/components/model_evaluator_and_pusher.py
@@ -25,8 +25,8 @@ eval_config = tfma.EvalConfig(
     slicing_specs=[
        tfma.SlicingSpec(),
        tfma.SlicingSpec(feature_keys=['Gender']),
-        # tfma.SlicingSpec(feature_keys=['Age']),
-        # tfma.SlicingSpec(feature_keys=['City_Category'])
+        tfma.SlicingSpec(feature_keys=['Age']),
+        tfma.SlicingSpec(feature_keys=['City_Category'])
     ],
     metrics_specs=[
         tfma.MetricsSpec(
@@ -55,17 +55,17 @@ serving_image = "europe-docker.pkg.dev/vertex-ai-restricted/prediction/tf_opt-cp
 
 def create_evaluator_and_pusher(example_gen, trainer, serving_model_dir):
 
-    # model_resolver = tfx.dsl.Resolver(
-    # strategy_class=tfx.dsl.experimental.LatestBlessedModelStrategy,
-    # model=tfx.dsl.Channel(type=tfx.types.standard_artifacts.Model),
-    # model_blessing=tfx.dsl.Channel(
-    #     type=tfx.types.standard_artifacts.ModelBlessing)).with_id(
-    #         'latest_blessed_model_resolver')
+    model_resolver = tfx.dsl.Resolver(
+    strategy_class=tfx.dsl.experimental.LatestBlessedModelStrategy,
+    model=tfx.dsl.Channel(type=tfx.types.standard_artifacts.Model),
+    model_blessing=tfx.dsl.Channel(
+        type=tfx.types.standard_artifacts.ModelBlessing)).with_id(
+            'latest_blessed_model_resolver')
 
     evaluator = Evaluator(
         examples=example_gen.outputs['examples'],
         model=trainer.outputs['model'],
-        # baseline_model=model_resolver.outputs['model'],
+        baseline_model=model_resolver.outputs['model'],
         eval_config=eval_config,
         example_splits=['test']
     )
@@ -83,4 +83,4 @@ def create_evaluator_and_pusher(example_gen, trainer, serving_model_dir):
           tfx.extensions.google_cloud_ai_platform.SERVING_ARGS_KEY:
             vertex_serving_spec,
       })
-    return evaluator, pusher
+    return evaluator, pusher, model_resolver
